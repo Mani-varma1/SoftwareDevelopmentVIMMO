035a8d2319d23171b397000cc21aa03208240e40
OllieGYoung <oliveryoung@ntlworld.com>
2024-12-01 23:15:16 +0000

    Update panelapp.py - Addition of get_latest_online_version() function

--- vimmo/utils/panelapp.py
+++ vimmo/utils/panelapp.py
@@ -29,3 +29,38 @@ class PanelAppClient:
         json_data = self._check_response(url)
         gene_symbols = [entry["gene_data"]["gene_symbol"] for entry in json_data.get("results", [])]
         return gene_symbols
+    
+    def get_latest_online_version(self, panel_id: str) -> str: 
+        """
+        Returns the most recent signedoff panel version from the panelapp api
+
+        Parameters
+        ----------
+        panel_id : str
+        The panel to search for in the panelapp database
+
+        Returns
+        -------
+        version: str
+        The latest  version for the most recent GMS signedoff panelapp panel
+
+        Notes
+        -----
+        - This function substitues the input panel id into the signedoff URL
+        - Using the response module, sends and recieves a GET HTTP request and response
+        - It extracts the .json response format
+        - Indexs the results nested dictionary, and extracts the 'version'
+
+        Example
+        -----
+        User UI input: R208
+        Query class method: rcode_to_panelID(R208) -> 635 # converts rcode to panel_id (see db.py)
+        get_latest_online_version(635) -> 2.5
+        
+        Here 2.5 is the version of R208, as of (26/11/24)
+        """
+        url = f'{self.base_url}/signedoff/?panel_id={panel_id}&display=latest' # Set the URL 
+        json_data = self._check_response(url) # Send get request to URL, if 200 return json format of the response
+        version = json_data["results"][0]["version"] # Extract the version number from the json response
+
+        return version
