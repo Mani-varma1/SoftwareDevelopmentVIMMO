8fea5963f66fdf988d4f2608bbc8ef969d52653f
Itebbs22 <iwantebbs@hotmail.co.uk>
2024-12-02 12:44:07 +0000

    previous commit didnt add all updates - updated weekly_update script and added a test for the script to dial back panel 3 and 9 versions

--- database_prework/createdb/create_newdb.py
+++ database_prework/createdb/create_newdb.py
@@ -62,6 +62,18 @@ CREATE TABLE IF NOT EXISTS patient_data (
 )
 ''')
 
+# Create Table 5: archive panel_genes with Panel_ID, HGNC_ID, Version and Confidence
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS panel_genes_archive (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Panel_ID INTEGER,
+    HGNC_ID TEXT,
+    Version TEXT,
+    Confidence INTEGER,
+    FOREIGN KEY (Panel_ID) REFERENCES panel (Panel_ID)
+)
+''')
+
 # Populate Table 1: panel
 df_panel.to_sql('panel', conn, if_exists='replace', index=False)
 
