e7d3686f7e3bb4310300d4af4791ad67da6f3f63
Vincent <vincent.umelue@nhs.net>
2024-12-06 11:39:19 +0000

    logging configuration

---
+++ logs/Sprint - NGTD/configure.py
@@ -0,0 +1,20 @@
+import logging
+
+# Logging Configuration
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s]: %(message)s",
+    # filename="logs/app.log",
+    # filemode="a",
+    handlers=[
+        logging.FileHandler("logs/app.log"),
+        logging.StreamHandler()
+    ]
+)
+
+# Logger creation
+logger = logging.getLogger("VIMMO")
+
+# Using the logger to log messages
+logger.debug("This is a debug message")
