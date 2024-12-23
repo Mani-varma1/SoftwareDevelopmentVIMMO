a334534c7413e87bbc36ba64fa167c25dd83302c
Itebbs22 <133106440+Itebbs22@users.noreply.github.com>
2024-12-23 14:18:32 +0000

    Update main.py

--- vimmo/main.py
+++ vimmo/main.py
@@ -1,14 +1,7 @@
-import os
 from vimmo.API import app
 
-def get_gateway_ip():
-    # Run the Docker network inspect command to get the gateway IP
-    gateway_ip = os.popen("docker network inspect bridge --format='{{(index .IPAM.Config 0).Gateway}}'").read().strip()
-    return gateway_ip
-
 def main():
-    gateway_ip = get_gateway_ip()  # Fetch the gateway IP dynamically
-    app.run(host=gateway_ip, port=5001, debug=True)
+    app.run(host=172.17.0.1, port=5001, debug=True)
 
 if __name__ == "__main__":
     main()
