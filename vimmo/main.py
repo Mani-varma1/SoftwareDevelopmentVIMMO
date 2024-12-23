225f7a9c7530d36e5b3300db3b86bef0f0b2d0f0
Itebbs22 <iwantebbs@hotmail.co.uk>
2024-12-23 13:29:09 +0000

    ..

--- vimmo/main.py
+++ vimmo/main.py
@@ -1,7 +1,8 @@
 from vimmo.API import app
+import os
 
 def main():
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host=os.environ.get("BACKEND_HOST", "127.0.0.1"), port=5001, debug=True)
 
 if __name__ == '__main__':
     main()
